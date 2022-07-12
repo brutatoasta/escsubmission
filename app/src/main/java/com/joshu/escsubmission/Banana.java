6c4b884e0e3eeb1e3805de783c21003bac89dffe
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-07-12 16:40:39 +0800

    Change writeCSVTest in AppleUnitTest to use temporary file

---
+++ app/src/main/java/com/joshu/escsubmission/Banana.java
@@ -0,0 +1,12 @@
+package com.joshu.escsubmission;
+
+import java.util.Arrays;
+
+// Garbage file to test random stuff
+public class Banana {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+        System.out.println("\"Starfruit\", \"Hl\"");
+        System.out.println(String.valueOf(System.currentTimeMillis()));
+    }
+}
--- app/src/test/java/com/joshu/escsubmission/AppleUnitTest.java
+++ app/src/test/java/com/joshu/escsubmission/AppleUnitTest.java
@@ -7,13 +7,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -22,28 +29,52 @@ public class AppleUnitTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
+
     /**
+     * Read a static sample file, check the first row
+     *
      * @throws IOException
      */
     @Test
     public void readCSVTest() throws IOException {
         String filename = "C:\\Users\\joshu\\Documents\\Github\\escsubmission\\sample_file_output_comparing_1_and_3.csv";
-
         ArrayList<String> arr = readCSV(filename);
-        //System.out.println(arr.get(0));
-        //System.out.println("\"ID99\",\"BOS8059799\",\"SGD\",\"CURRENT\",\"208043\"");
         assert(arr.get(0).equals("\"ID99\",\"BOS8059799\",\"SGD\",\"CURRENT\",\"208043\"") );
     }
 
     /**
+     * Creates a temporary copy of writetest0.csv, with a single entry.
+     * Adds 2 lines of entries and check if the hashset that is read back is correct
+     *
      * @throws IOException
      */
     @Test
     public void writeCSVTest() throws IOException{
+        // copy file
+        File copied =  folder.newFile("testTemp.csv");
+        String original = "C:\\Users\\joshu\\Documents\\Github\\escsubmission\\writetest0.csv";
+        String intendedfilename = "C:\\Users\\joshu\\Documents\\Github\\escsubmission\\writetest1.csv";
+        try (
+                InputStream in = new BufferedInputStream(
+                        new FileInputStream(original));
+                OutputStream out = new BufferedOutputStream(
+                        new FileOutputStream(copied))) {
 
-        //use an existing file
-        String filename = "C:\\Users\\joshu\\Documents\\Github\\escsubmission\\writetest0.csv";
-        File file = new File(filename);
+            byte[] buffer = new byte[1024];
+            int lengthRead;
+            while ((lengthRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, lengthRead);
+                out.flush();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+        assert(copied.exists());
+        assert(Files.readAllLines(Paths.get(original))
+                .equals(Files.readAllLines(copied.toPath())));
+
         HashSet<String> result = new HashSet<>();
 
         //intended answer
@@ -60,14 +91,14 @@ public class AppleUnitTest {
         }};
 
         // run the function we want to test
-        writeCSV(toAdd, file);
+        writeCSV(toAdd, copied);
 
         // time to verify: read the file
         // TODO: very similar code to readCSV function I wrote, is this okay?
         try {
             FileInputStream fis = null;
             try {
-                fis = new FileInputStream(file);
+                fis = new FileInputStream(copied);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
